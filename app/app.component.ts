bc56985f80e99ba3d944370373e73c012593fb92
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-24 16:28:51 +0530

    Code

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,11 +7,18 @@ import { Component } from '@angular/core';
   styleUrls:['./app.component.css']
 })
 export class AppComponent{
+  salary:number = 21233.98765;
+  obj = {id:1001, name:'Ram',salary:9999};
+  fruits:string[] = ["Apple","Banana","Orange"];
   msg:string;
   cssclass:string ='green';
   isDisabled:boolean = true;
+  result:number = 0;
   constructor(){
     this.msg = 'Hello Angular';
+  }
+  add(a:string, b:string){
+    this.result = parseInt(a) + parseInt(b);
   }
   takeInput(event):void{
     this.msg = event.target.value;
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations:[AppComponent],
-  imports:[BrowserModule],
+  imports:[BrowserModule,FormsModule],
   providers:[],
   bootstrap:[AppComponent]
 })
